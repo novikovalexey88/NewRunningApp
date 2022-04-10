99d585567e3252ffc786d82be66d98c90c5e4a89
novikovalexey88 <novikov.a.m@yandex.ru>
2022-04-10 18:51:12 +0300

    Run

--- app/src/main/java/com/example/newrunningapp/ui/fragments/TrackingFragment.kt
+++ app/src/main/java/com/example/newrunningapp/ui/fragments/TrackingFragment.kt
@@ -1,12 +1,69 @@
 package com.example.newrunningapp.ui.fragments
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import com.example.newrunningapp.R
 import com.example.newrunningapp.ui.viewmodels.MainViewModel
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.MapView
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class TrackingFragment: Fragment(R.layout.fragment_tracking) {
     private val viewModel: MainViewModel by viewModels()
+
+    private  var map: GoogleMap? = null
+   private lateinit var mapView: MapView
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mapView = view.findViewById(R.id.mapView)
+
+        mapView.onCreate(savedInstanceState)
+
+        mapView.getMapAsync {
+            map = it
+        }
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView?.onResume()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        mapView?.onStart()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        mapView?.onStop()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        mapView?.onPause()
+    }
+
+    override fun onLowMemory() {
+        super.onLowMemory()
+        mapView?.onLowMemory()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        mapView?.onDestroy()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        mapView?.onSaveInstanceState(outState)
+    }
+
 }
+
